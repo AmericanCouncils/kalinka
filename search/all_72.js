51e2f29c804daf3a4a0039a0993bd1891181ef63
David Simon <dsimon@americancouncils.org>
2013-02-21 17:29:24 -0500

    Updated docs

--- search/all_72.js
+++ search/all_72.js
@@ -1,6 +1,9 @@
 var searchData=
 [
   ['readme',['README',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['registeractions',['registerActions',['../class_authorizer_1_1_authorizer_abstract.html#aef5c0150c93d30ffb13fc3e323028c86',1,'Authorizer::AuthorizerAbstract']]],
   ['registerguards',['registerGuards',['../class_authorizer_1_1_authorizer_abstract.html#ad9c78522a66264ed0220d9baa9eedfba',1,'Authorizer::AuthorizerAbstract']]],
+  ['registerpolicies',['registerPolicies',['../class_authorizer_1_1_simple_authorizer.html#a13673a89347afefd528bcef2feb077bb',1,'Authorizer::SimpleAuthorizer']]],
+  ['registerrolepolicies',['registerRolePolicies',['../class_authorizer_1_1_role_authorizer.html#ab3d09dd2ded81b29a9f72b698aa0d2b2',1,'Authorizer::RoleAuthorizer']]],
   ['roleauthorizer',['RoleAuthorizer',['../class_authorizer_1_1_role_authorizer.html',1,'Authorizer']]]
 ];
--- search/functions_72.js
+++ search/functions_72.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['registerguards',['registerGuards',['../class_authorizer_1_1_authorizer_abstract.html#ad9c78522a66264ed0220d9baa9eedfba',1,'Authorizer::AuthorizerAbstract']]]
+  ['registeractions',['registerActions',['../class_authorizer_1_1_authorizer_abstract.html#aef5c0150c93d30ffb13fc3e323028c86',1,'Authorizer::AuthorizerAbstract']]],
+  ['registerguards',['registerGuards',['../class_authorizer_1_1_authorizer_abstract.html#ad9c78522a66264ed0220d9baa9eedfba',1,'Authorizer::AuthorizerAbstract']]],
+  ['registerpolicies',['registerPolicies',['../class_authorizer_1_1_simple_authorizer.html#a13673a89347afefd528bcef2feb077bb',1,'Authorizer::SimpleAuthorizer']]],
+  ['registerrolepolicies',['registerRolePolicies',['../class_authorizer_1_1_role_authorizer.html#ab3d09dd2ded81b29a9f72b698aa0d2b2',1,'Authorizer::RoleAuthorizer']]]
 ];
