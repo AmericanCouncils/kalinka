68a37389c24d5aa1099a3458948f1d0ad31fb724
David Simon <dsimon@americancouncils.org>
2013-02-22 13:02:34 -0500

    Update

--- search/functions_67.js
+++ search/functions_67.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['getpermission',['getPermission',['../class_authorizer_1_1_authorizer_abstract.html#a4f34bed07737f0c6fc797e434570d8d6',1,'Authorizer\AuthorizerAbstract\getPermission()'],['../class_authorizer_1_1_role_authorizer.html#a2f8dab8a697eefcd465158d0c74a4b87',1,'Authorizer\RoleAuthorizer\getPermission()'],['../class_authorizer_1_1_simple_authorizer.html#ad6f8ba37752aec9d019543b61097b173',1,'Authorizer\SimpleAuthorizer\getPermission()']]]
+  ['getpermission',['getPermission',['../class_authorizer_1_1_authorizer_abstract.html#a4f34bed07737f0c6fc797e434570d8d6',1,'Authorizer\AuthorizerAbstract\getPermission()'],['../class_authorizer_1_1_role_authorizer.html#a2f8dab8a697eefcd465158d0c74a4b87',1,'Authorizer\RoleAuthorizer\getPermission()'],['../class_authorizer_1_1_simple_authorizer.html#ad6f8ba37752aec9d019543b61097b173',1,'Authorizer\SimpleAuthorizer\getPermission()']]],
+  ['getrolepermission',['getRolePermission',['../class_authorizer_1_1_role_authorizer.html#aa9efe9f77b72ba9e7c210ccf239b4e5b',1,'Authorizer::RoleAuthorizer']]]
 ];
---
+++ search/functions_68.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hasrole',['hasRole',['../class_authorizer_1_1_role_authorizer.html#a304f63e2398c6b73eea3095df82f13bf',1,'Authorizer::RoleAuthorizer']]]
+];
